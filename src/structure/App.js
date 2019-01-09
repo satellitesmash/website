db34260e6a478ab6217ce06e6884f2aef6c55332
Jonathan Cady <joncady@uw.edu>
2019-01-08 22:42:30 -0800

    Remvoved browser router

--- src/structure/App.js
+++ src/structure/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Footer from '../components/Footer';
 import ContentSwitch from './ContentSwitch';
 import NavBar from '../components/NavBar';
@@ -8,7 +8,7 @@ export default class App extends Component {
 
     render() {
         return(
-            <Router basename={process.env.PUBLIC_URL}>
+            <Router>
                 <div>
                     <NavBar/>
                     <ContentSwitch/>
